694e3864365e030633a09d4d7b776f4b264b9d9a
ismaileneskaya@gmail.com <ismaileneskaya@gmail.com>
2024-03-19 19:41:42 +0300

    1.completed

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,4 +4,17 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
+
+    public static boolean isPalindrome(int number){
+        number= Math.abs(number);
+        char[] digits =String.valueOf(number).toCharArray();
+        String reversed="";
+        for (int i = digits.length-1; i>=0 ; i--) {
+            reversed += digits[i];
+        }
+        return reversed.equals(String.valueOf(number));
+    }
+
+    public
+
 }
